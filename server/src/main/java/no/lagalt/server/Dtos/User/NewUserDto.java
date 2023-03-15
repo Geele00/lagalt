7f2559df8acb9658977f1f371f35aa32a781fc4f
Wes Keiser <wes@weskeiser.no>
2023-03-15 19:00:04 +0100

    fix: correct date format

--- server/src/main/java/no/lagalt/server/Dtos/User/NewUserDto.java
+++ server/src/main/java/no/lagalt/server/Dtos/User/NewUserDto.java
@@ -1,5 +1,6 @@
 package no.lagalt.server.Dtos.User;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -22,7 +23,7 @@ public class NewUserDto {
 
   private String lastName;
 
-  private LocalDateTime dob;
+  private LocalDate dob;
 
   private String email;
 
