df1d1685b9ec3356c2c5c02c703d0f1f51b588ff
Wes Keiser <wes@weskeiser.no>
2023-03-16 16:47:59 +0100

    feat: add search result component

---
+++ client/src/features/SearchBar/SearchResult/SearchResult.tsx
@@ -0,0 +1,13 @@
+import "./SearchResult.style.scss";
+
+export const SearchResult = () => {
+  return (
+    <li className="search-result">
+      <img src="/images/comment.svg" alt="Thumbnail" />
+      <p>Project in search result</p>
+      <div>
+        <p>Coding</p>
+      </div>
+    </li>
+  );
+};
