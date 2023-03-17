c6748a3067da4b848db3e1283b021394b809588d
Geele Egal <geele.egalno.experis.com>
2023-03-17 10:46:38 +0100

    SubHeader

--- client/src/App/App.tsx
+++ client/src/App/App.tsx
@@ -1,11 +1,13 @@
 import "./App.style.scss";
 import { Outlet } from "@tanstack/react-router";
 import { Header } from "src/features/Header/Header";
+import SubHeader from "src/features/SubHeader/SubHeader";
 
 const App = () => {
   return (
     <>
       <Header />
+      <SubHeader />
       <main>
         <Outlet />
       </main>
--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO lagalt_user(bio, email, first_name, last_name,gender, username, created_at, dob, profile_status, uid)
 VALUES
-('test', 'test@test.com', 'Abbas', 'Shihab', 0,'user1', '06-27-1994', '06-27-1994', 1, 'tempUID1'),
+('test', 'test@test.com', 'Abbas', 'Shihab', 0,'user1', '01-01-1994', '01-01-1994', 1, 'tempUID1'),
 ('Wes bio section', 'lkjlkj@gmail.com', 'Wes', 'Keiser', 0, 'weskeiser', '01-01-2000', '01-01-2000', 1, 'Ewe103Eg10WHbXvXcWbnUHpROLF2'),
 ('test', 'test@test.com', 'Maria', 'J', 1,'user3', '01-01-2000', '01-01-2000', 1, 'tempUID3'),
 ('test', 'test@test.com', 'Geele', 'Egal', 0,'user4', '01-01-2000', '01-01-2000', 1, 'tempUID4'),
