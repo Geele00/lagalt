fd4f3a800cf282eb1d5490e4ce83451fec872bbf
Wes Keiser <wes@weskeiser.no>
2023-03-19 15:14:01 +0100

    refactor: revert fetch logic

--- client/src/api/v1/chats/Chats.ts
+++ client/src/api/v1/chats/Chats.ts
@@ -6,16 +6,18 @@ const endpoint = import.meta.env.VITE_API_V1_URL + "/chats";
 export const sendChatMessageFetch = async (
   body: SendMessage,
   token: string
-): Promise<IChatMessagePage[]> =>
-  fetch(`${endpoint}`, {
+): Promise<IChatMessagePage[]> => {
+  const res = await fetch(`${endpoint}`, {
     method: "POST",
     body: JSON.stringify(body),
     headers: {
-      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
-  })
-    .then((res) => res.json())
-    .catch((err) => {
-      throw new Error(err.statusText);
-    });
+  });
+
+  if (!res.ok) {
+    return Promise.reject(res);
+  }
+
+  return res.json();
+};
