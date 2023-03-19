6b57a0b1aa573fa25e56d6facf12b31ab2b97285
Wes Keiser <wes@weskeiser.no>
2023-03-19 15:18:14 +0100

    wip: work on search

--- server/src/main/java/no/lagalt/server/Controller/SearchController.java
+++ server/src/main/java/no/lagalt/server/Controller/SearchController.java
@@ -2,6 +2,7 @@ package no.lagalt.server.Controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
 import no.lagalt.server.Dtos.Page.PageDto;
 import no.lagalt.server.Dtos.Search.ProjectSearchResult;
 import no.lagalt.server.Service.*;
@@ -20,7 +21,11 @@ public class SearchController {
   @Operation(summary = "Search the database")
   @GetMapping
   PageDto<ProjectSearchResult> getSearchResult(
-      @RequestParam(name = "query", required = true) String query, Authentication auth) {
+      @RequestParam(name = "query", required = false) String query,
+      @RequestParam(name = "skill", required = false) List<String> skills,
+      @RequestParam(name = "industries", required = false) List<String> industries,
+      @RequestParam(name = "users", required = false) boolean users,
+      Authentication auth) {
 
     String uid = auth.getName();
 
