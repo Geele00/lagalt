7b3db1c25778db2a4a2f22dd67823112c54f69ee
Geele Egal <geele.egalno.experis.com>
2023-03-20 11:17:33 +0100

    pop up animation and banner design. removed api

--- client/src/features/Explore/Explore.tsx
+++ client/src/features/Explore/Explore.tsx
@@ -1,9 +1,12 @@
 import "./Explore.style.scss";
+import NewsFeed from "src/components/NewsApi/NewsFeed";
 
 export const Explore = () => {
   return (
     <section className="explore">
-      <p>Utforsk kreative prosjekter</p>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repudiandae expedita doloribus iste unde aperiam saepe temporibus. Optio ab, ipsam alias illum cupiditate error doloribus at nihil saepe impedit voluptates distinctio.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus dolorem, veniam perspiciatis facilis fugit repellat ratione ab, esse laudantium deserunt omnis eos molestias, velit voluptatem enim laborum qui optio vero! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur blanditiis, libero voluptas nam tempore suscipit tempora ipsam voluptate nemo odit molestias nulla unde veritatis cumque exercitationem iste dolore? Facere, maxime? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic tenetur iste voluptas. Aliquid molestias aliquam impedit, velit dicta repellat nam aperiam, vero repellendus architecto facere reprehenderit assumenda nesciunt earum in! Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam ratione veniam praesentium laborum incidunt optio et officiis consectetur quis eos aut, cumque, quo non maxime perferendis, temporibus illum fuga! Expedita. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis culpa deserunt voluptatem praesentium voluptatum beatae fuga, nulla consequatur nam, doloremque iste, eligendi error assumenda in qui possimus accusamus. Asperiores, nam. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt impedit numquam consectetur blanditiis quisquam expedita dolore magni veritatis quod, et similique provident exercitationem reiciendis cupiditate fuga. Dolore dicta aperiam recusandae?
+      </p>
     </section>
   );
 };
