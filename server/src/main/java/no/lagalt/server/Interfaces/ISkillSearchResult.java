17168ab482de9b7a0dfe46809c04afb99fb25d02
Wes Keiser <wes@weskeiser.no>
2023-03-21 14:42:33 +0100

    wip: improving skill searching

---
+++ server/src/main/java/no/lagalt/server/Interfaces/ISkillSearchResult.java
@@ -0,0 +1,9 @@
+package no.lagalt.server.Interfaces;
+
+public interface ISkillSearchResult {
+  Integer getSkillId();
+
+  String getName();
+
+  String getAddedBy();
+}
--- server/src/main/java/no/lagalt/server/Mapper/SkillMapper.java
+++ server/src/main/java/no/lagalt/server/Mapper/SkillMapper.java
@@ -3,6 +3,7 @@ package no.lagalt.server.Mapper;
 import java.util.List;
 import no.lagalt.server.Dtos.Skill.SkillDto;
 import no.lagalt.server.Entity.Skill;
+import no.lagalt.server.Interfaces.ISkillSearchResult;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -17,4 +18,6 @@ public interface SkillMapper {
   SkillDto toDto(Skill skill);
 
   List<SkillDto> toDto(List<Skill> skill);
+
+  List<SkillDto> searchResultToDto(List<ISkillSearchResult> searchResult);
 }
