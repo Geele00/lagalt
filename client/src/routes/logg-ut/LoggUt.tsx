2ea9792ecc27305b2697c731e7c2c3816947be4d
Geele Egal <geele.egalno.experis.com>
2023-03-26 00:12:04 +0100

    LoggUt page. filter background

--- client/src/routes/logg-ut/LoggUt.tsx
+++ client/src/routes/logg-ut/LoggUt.tsx
@@ -1,6 +1,7 @@
 import { Link } from "@tanstack/react-router";
 import { useEffect } from "react";
 import { useAuth } from "src/auth/Auth.Provider";
+import "./LoggUtStyle.scss"
 
 const LoggUt = () => {
   const { signOut } = useAuth();
@@ -8,10 +9,15 @@ const LoggUt = () => {
   useEffect(() => signOut(), []);
 
   return (
-    <div>
-      <p>Signout logic</p>
-      <Link to="/">Ta meg til forsiden</Link>
+    <div className="log-out-page">
+      <div className="log-out-page-container">
+      <title>Logget ut av LagAlt</title>
+      <h1 className="log-out-page-tile">Du har blitt logget ut</h1>
+      <p className="log-out-page-paragraph">Takk for at du brukte vår tjeneste</p>
+      <Link className="log-out-page-link" to="/">Ta meg til forsiden</Link>
     </div>
+    </div>
+    
   );
 };
 export default LoggUt;
